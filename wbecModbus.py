0f82ec0ceb86d00be5499c8c40443147a8ae65b0
steff393 <stefan.ferstl@gmail.com>
2023-06-03 14:56:58 +0200

    Add command line parameters

--- wbecModbus.py
+++ wbecModbus.py
@@ -11,23 +11,32 @@ print("wbec Modbus-TCP-Kompatibilitätsprüfung")
 config = configparser.ConfigParser(inline_comment_prefixes='#')
 config.read('cfg.ini')
 
-cfg = config['Configuration']
-typ = config['Configuration']['Typ']
+if len(sys.argv) == 3:
+	print("Übernehme IP und Typ aus Aufrufparametern...")
+	modbusIP  = sys.argv[1]
+	modbusTyp = sys.argv[2]
+	endEnter  = False
+else:
+	print("Übernehme IP und Typ aus cfg.ini...")
+	modbusIP  = config['Configuration']['IP']
+	modbusTyp = config['Configuration']['Typ']
+	endEnter  = True
 
-for key in cfg:
-	print(key.ljust(18) + ": " + cfg[key])
-print('Port'.ljust(18) + ": " + config[typ]['Port'])
+print('IP   : ' + modbusIP)
+print('Typ  : ' + modbusTyp)
+print('Port : ' + config[modbusTyp]['Port'])
+print('Addr : ' + config[modbusTyp]['Addr'])
 print("...bitte warten...")
 
 try:
-	client = ModbusTcpClient(cfg['cfgInverterIp'], port=config[typ]['Port'])
+	client = ModbusTcpClient(modbusIP, port=config[modbusTyp]['Port'])
 	client.connect()
 	print("Registerwerte:")
-	for key in config[typ]:
+	for key in config[modbusTyp]:
 		if not key.isdigit():
-			continue
-		# If key contains a register number, then read it
-		response = client.read_holding_registers(int(key), int(config[typ][key]), slave=int(cfg['cfgInverterAddr']), unit=0x01)
+			continue  # If key doesn't contains a register number, then skip it
+		
+		response = client.read_holding_registers(int(key), int(config[modbusTyp][key]), slave=int(config[modbusTyp]['Addr']), unit=0x01)
 
 		# Check if reading was successful
 		if response.isError():
@@ -37,7 +46,10 @@ try:
 				print(f"Register {int(key) + i}: {register_value}")
 
 	client.close()
+	print("Server " + modbusIP + ", Port " + config[modbusTyp]['Port'] + " erfolgreich ausgelesen")
 
 except ConnectionException:
-	print("Fehler: Server " + cfg['cfgInverterIp'] + ", Port " + config[typ]['Port'] + " nicht erreichbar")
+	print("Fehler: Server " + modbusIP + ", Port " + config[modbusTyp]['Port'] + " nicht erreichbar")
 
+if endEnter:
+	input("<Enter> zum Beenden...")
